dd67748ad2ccf074e8452178ae462e21d8cc0d50
GEONWOO <rjsdn736@naver.com>
2025-01-23 16:46:15 +0900

    feat : 채팅 html 파일 생성 및 프로젝트 구조 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ChatModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),  // 정적 파일 경로 설정
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
